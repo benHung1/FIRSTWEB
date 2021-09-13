59a71f001c546d1aef727e6a142cabec61d73157
benHung1 <ga2006206521@gmail.com>
2021-09-14 01:49:27 +0800

    FINALLY1

--- GOGO/js/experience.js
+++ GOGO/js/experience.js
@@ -87,3 +87,10 @@ $('.slider').each(function() {
     
     advance();
   });
+
+  $(function(){
+    // 點擊按鈕，選單縮放
+    $("button.btn_switch").on("click", function(){
+    $("nav.nav").slideToggle();
+});
+});
