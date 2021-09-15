2190ce96c235aac044517014ae2e6ba7d9449a54
benHung1 <ga2006206521@gmail.com>
2021-09-15 11:05:28 +0800

    finfnfnnf

--- GOGO/js/explore.js
+++ GOGO/js/explore.js
@@ -5,16 +5,6 @@ $(function(){
 });
 });
 
-window.addEventListener('scroll', ()=>{
-    let content = document.querySelector('.container');
-    let contentPosition = content.getBoundingClientRect().top;
-    let screenPosition = window.innerHeight;
-    if(contentPosition < screenPosition){
-        content.classList.add('active');
-    }else{
-        content.classList.remove('active');
-    }
-});
 
 window.addEventListener('scroll', ()=>{
     let content = document.querySelector('.container1');
